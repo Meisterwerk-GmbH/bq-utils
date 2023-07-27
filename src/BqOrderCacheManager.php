155eadd78dd0e6a554aca568c82e118e4e83681e
Sara Lüthi <sara.luethi@students.bfh.ch>
2023-07-27 11:41:03 +0200

    fix typo

--- src/BqOrderCacheManager.php
+++ src/BqOrderCacheManager.php
@@ -8,8 +8,8 @@ class BqOrderCacheManager
 {
     private mixed $order = null;
 
-    // filehandle is a resource; this is a special variable, holding a reference to an external resource
-    private $filehandle;
+    // fileHandle is a resource; this is a special variable, holding a reference to an external resource
+    private $fileHandle;
 
     private string $cachePath;
 
@@ -31,7 +31,7 @@ class BqOrderCacheManager
         if (!file_exists($orderPath)) {
             file_put_contents($orderPath, json_encode((object)[]));
         }
-        $this->filehandle = fopen($orderPath, 'r+');
+        $this->fileHandle = fopen($orderPath, 'r+');
         $this->order = $this->readAndLock($orderPath);
         return $this->order;
     }
@@ -43,16 +43,16 @@ class BqOrderCacheManager
 
     public function writeAndUnlock(): void
     {
-        ftruncate($this->filehandle, 0);    //Truncate the file to 0
-        rewind($this->filehandle);           //Set write pointer to beginning of file
-        fwrite($this->filehandle, json_encode($this->order, JSON_PRETTY_PRINT));    //Write the new Hit Count
-        flock($this->filehandle, LOCK_UN);
+        ftruncate($this->fileHandle, 0);    //Truncate the file to 0
+        rewind($this->fileHandle);           //Set write pointer to beginning of file
+        fwrite($this->fileHandle, json_encode($this->order, JSON_PRETTY_PRINT));    //Write the new Hit Count
+        flock($this->fileHandle, LOCK_UN);
     }
 
     private function readAndLock($orderPath)
     {
-        if (flock($this->filehandle, LOCK_EX)) {
-            return json_decode(fread($this->filehandle, filesize($orderPath)));
+        if (flock($this->fileHandle, LOCK_EX)) {
+            return json_decode(fread($this->fileHandle, filesize($orderPath)));
         }
         throw new \Exception('FileHandle could not be locked!');
     }
