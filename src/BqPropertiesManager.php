ce36531e5cc5b7727c740f19dcd6247fd403f2a5
Sara Lüthi <sara.luethi@students.bfh.ch>
2023-07-27 11:28:53 +0200

    use RestManager in PropertiesManager

--- src/BqPropertiesManager.php
+++ src/BqPropertiesManager.php
@@ -4,32 +4,40 @@ namespace Meisterwerk\BqUtils;
 
 class BqPropertiesManager
 {
-    private const BQ_API_PATH_1 = 'https://rentshop.booqable.com/api/1/';
-    private const BQ_API_PATH_3 = 'https://rentshop.booqable.com/api/3/';
+    private const BQ_API_PATH_V1 = 'https://rentshop.booqable.com/api/1/';
+    private const BQ_API_PATH_V3 = 'https://rentshop.booqable.com/api/3/';
+
+    private BqRestManager $bqRestManagerV1;
+
+    private BqRestManager $bqRestManagerV3;
+
+    public function __construct(string $apiKey)
+    {
+        $this->bqRestManagerV1 = new BqRestManager($apiKey, self::BQ_API_PATH_V1);
+        $this->bqRestManagerV3 = new BqRestManager($apiKey, self::BQ_API_PATH_V3);
+    }
 
     /**
      * @param array $propertyFields example: $propertyFields = ['identifier' => 'sprache', 'name' => 'Sprache',]
      * @throws BqRequestException
      */
-    public static function createOrUpdateProperty($bqOrderId, $trackingNumber, array $propertyFields, $bqApiKey): void
+    public function createOrUpdateProperty($bqOrderId, $trackingNumber, array $propertyFields): void
     {
-        $properties = self::getProperties($bqOrderId, $bqApiKey)->data;
+        $properties = self::getProperties($bqOrderId)->data;
         $filteredProperties = array_filter($properties, fn($property) => $property->attributes->name === $propertyFields['name']);
         $propertyToSet = array_pop($filteredProperties);
         if ($propertyToSet === null) {
             self::createProperty(
                 $trackingNumber,
                 $bqOrderId,
-                $propertyFields['identifier'],
-                $bqApiKey
+                $propertyFields['identifier']
             );
         } else {
             self::updateProperty(
                 $propertyToSet->attributes->value . $trackingNumber,
                 $bqOrderId,
                 $propertyFields['identifier'],
-                $propertyToSet->id,
-                $bqApiKey
+                $propertyToSet->id
             );
         }
     }
@@ -37,66 +45,45 @@ class BqPropertiesManager
     /**
      * @throws BqRequestException
      */
-    private static function getProperties(string $orderId, string $bqApiKey)
+    private function getProperties(string $orderId)
     {
-        return BqUtil::request([
-            CURLOPT_URL => self::BQ_API_PATH_3 . 'properties?filter[owner_id]=' . $orderId,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => [
-                'Authorization: Bearer ' . $bqApiKey
-            ],
-        ]);
+        return $this->bqRestManagerV3->get('properties?filter[owner_id]=' . $orderId);
     }
 
     /**
      * @throws BqRequestException
      */
-    private static function createProperty($value, $orderId, $propertyIdentifier, $bqApiKey): void
+    private function createProperty($value, $orderId, $propertyIdentifier): void
     {
-        BqUtil::request([
-            CURLOPT_URL => self::BQ_API_PATH_1.'properties',
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => json_encode([
-                'property' => [
-                    'identifier' => $propertyIdentifier,
-                    'value' => $value,
-                    'owner_id' => $orderId,
-                    'isNew' => 'true',
-                    'property_type' => 'Property::TextField',
-                    'type' => 'Property::TextField',
-                    'owner_type' => 'Order',
-                ],
-            ]),
-            CURLOPT_HTTPHEADER => [
-                'Authorization: Bearer '.$bqApiKey,
-                'Accept: application/json',
-                'Content-Type: application/json',
+        $postFields = [
+            'property' => [
+                'identifier' => $propertyIdentifier,
+                'value' => $value,
+                'owner_id' => $orderId,
+                'isNew' => 'true',
+                'property_type' => 'Property::TextField',
+                'type' => 'Property::TextField',
+                'owner_type' => 'Order',
             ],
-        ]);
+        ];
+        $this->bqRestManagerV1->post('properties', $postFields);
     }
 
     /**
      * @throws BqRequestException
      */
-    private static function updateProperty($value, $orderId, $propertyIdentifier, $propertyId, $bqApiKey): void
+    private function updateProperty($value, $orderId, $propertyIdentifier, $propertyId): void
     {
-        BqUtil::request([
-            CURLOPT_URL => self::BQ_API_PATH_1 . 'properties/' . $propertyId,
-            CURLOPT_CUSTOMREQUEST => 'PUT',
-            CURLOPT_POSTFIELDS => json_encode([
-                'property' => [
-                    'identifier' => $propertyIdentifier,
-                    'value' => $value,
-                    'owner_id' => $orderId,
-                    'property_type' => 'Property::TextField',
-                    'type' => 'Property::TextField',
-                    'owner_type' => "Order"
-                ]
-            ]),
-            CURLOPT_HTTPHEADER => [
-                'Authorization: Bearer ' . $bqApiKey,
-                'Content-Type: application/json'
-            ],
-        ]);
+        $postFields = [
+            'property' => [
+                'identifier' => $propertyIdentifier,
+                'value' => $value,
+                'owner_id' => $orderId,
+                'property_type' => 'Property::TextField',
+                'type' => 'Property::TextField',
+                'owner_type' => "Order"
+            ]
+        ];
+        $this->bqRestManagerV1->put('properties/' . $propertyId, $postFields);
     }
 }
